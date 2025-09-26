0d067a726c0861e747ae7ee8cf22a155ebc8a545
Rodrigo Pintos <rpintos@spacedev.io>
2025-09-26 16:32:29 -0300

    reorder tests

--- test/governance/proposal-manager.test.ts
+++ test/governance/proposal-manager.test.ts
@@ -1,8 +1,8 @@
 import { expect } from 'chai';
 import { ethers } from 'hardhat';
-import { DeploymentManager } from '../../../plugins/deployment_manager';
-import { ProposalManager, createProposalManager } from '../../../src/governor/helpers/proposalManager';
-import { ProposalAction, ProposalStack } from '../../../src/governor/types/proposalTypes';
+import { DeploymentManager } from '../../plugins/deployment_manager';
+import { ProposalManager, createProposalManager } from '../../src/governor/helpers/proposalManager';
+import { ProposalAction, ProposalStack } from '../../src/governor/types/proposalTypes';
 
 import * as fs from 'fs';
 import * as path from 'path';
